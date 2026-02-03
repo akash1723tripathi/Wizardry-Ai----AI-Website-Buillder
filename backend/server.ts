8d71ec4f765dbbe5dd3c6bff5851f6bd6915832e
akash1723tripathi <akashtripathi.acad@gmail.com>
2026-02-03 19:40:49 +0530

    Better-auth setup completed

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,8 @@
 import express ,{Request,Response} from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { auth } from './lib/auth.js';
+import { toNodeHandler } from 'better-auth/node';
 
 
 const app = express();
@@ -11,6 +13,8 @@ const corsOptions = {
       credentials : true,
 }
 app.use(cors(corsOptions));
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 app.get('/',(req:Request,res:Response)=>{
       res.send('Server is Live!');
